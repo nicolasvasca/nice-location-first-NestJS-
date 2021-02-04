c420fc438c9fe5806d224c6f465439edf13d7980
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-04 14:01:39 -0300

    feat: html handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {resolve } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const App_DIRECTORY = resolve(__dirname,'..');
+  const app = await NestFactory.create<NestApplication>(AppModule);
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(App_DIRECTORY,'views'));
+  app.useStaticAssets(resolve(App_DIRECTORY,'public'));
   await app.listen(3000);
 }
 bootstrap();
