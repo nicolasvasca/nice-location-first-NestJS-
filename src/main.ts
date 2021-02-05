463feffe057eace62ae819cee8004362db8a4eef
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-05 09:45:03 -0300

    refactor: main using config service

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {resolve } from 'path'
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const App_DIRECTORY = resolve(__dirname,'..');
   const app = await NestFactory.create<NestApplication>(AppModule);
   app.setViewEngine('hbs');
-  app.setBaseViewsDir(resolve(App_DIRECTORY,'views'));
-  app.useStaticAssets(resolve(App_DIRECTORY,'public'));
+  const config = app.get(ConfigService);
+  const viewsDirectory = config.get<string>('templates.path');
+  const publicDirectory = config.get<string>('public.path');
+  app.setBaseViewsDir(viewsDirectory);
+  app.useStaticAssets(publicDirectory);
+
   await app.listen(3000);
 }
 bootstrap();
