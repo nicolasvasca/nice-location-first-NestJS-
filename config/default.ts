907c29d9a463ab547671930a618b43948bf2f119
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-05 13:26:07 -0300

    feat: database config

--- config/default.ts
+++ config/default.ts
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
 
+const SOURCE_PATH = resolve(__dirname,'..','src');
+
 export default {
-    templates:{
-        path: resolve(__dirname,'..','views'),
+    database: {
+        url: 'postgres://app:app@localhost:5433/test',
+    },
+    orm: {
+        entities: [`${SOURCE_PATH}/**/*.entity.ts`],
+        synchornize: false,
     },
     public:{
         path: resolve(__dirname,'..','public')
-    }
+    },
+    templates:{
+        path: resolve(__dirname,'..','views'),
+    },
 }
