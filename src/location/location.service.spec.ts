7cbed07b05ba21348ab373fec9d2afb52ad76312
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-08 11:23:42 -0300

    test: location service using generic.factory

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Repository } from 'typeorm';
+import { Generated, Repository } from 'typeorm';
 import { Location } from './location.entity';
 import { LocationService } from './location.service';
 import * as sinon from 'sinon';
+import { GenericFactory } from '../common/generic.factory';
 
 describe('LocationService', () => {
   let service: LocationService;
@@ -16,12 +17,28 @@ describe('LocationService', () => {
     });
     mock = sinon.mock(mockRepository);
     mock.expects('find').resolves([
-
+      GenericFactory.create<Location>(Location, {
+        id: 'a2891a7f-df6f-4748-8bcc-5c669e5f9bb3',
+        name: 'Location',
+      }),
+      GenericFactory.create<Location>(Location, {
+        id: 'b313b81c-6ed0-4f91-a93b-91b9dd0655a1',
+        name: 'Location 2',
+      })
     ]);
     service = new LocationService(mockRepository);
   });
 
   it('should return locations', () => {
-    expect(service.list()).resolves.toMatchObject([])
+    expect(service.list()).resolves.toMatchObject([
+      {
+        id: 'a2891a7f-df6f-4748-8bcc-5c669e5f9bb3',
+        name: 'Location',
+      },
+      {
+        id: 'b313b81c-6ed0-4f91-a93b-91b9dd0655a1',
+        name: 'Location 2',
+      }
+    ])
   });
 });
