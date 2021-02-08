59317a9608da4a4464610a558f0168db33d1b557
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-08 12:05:32 -0300

    test: mock helper using in location service test

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,44 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { Generated, Repository } from 'typeorm';
-import { Location } from './location.entity';
 import { LocationService } from './location.service';
-import * as sinon from 'sinon';
+import { Repository } from 'typeorm';
+import { Location } from './location.entity';
 import { GenericFactory } from '../common/generic.factory';
+import { MockHelper } from '../common/mock.helper';
 
 describe('LocationService', () => {
   let service: LocationService;
   let mockRepository: Repository<Location>;
-  let mock: sinon.SinonMock;
 
   beforeEach(async () => {
-    mockRepository = { } as Repository<Location>;
-    mockRepository = Object.assign(mockRepository, {
-      find: new Function(),
+    mockRepository = MockHelper.mock<Repository<Location>>({
+      find: {
+        resolves: [
+          GenericFactory.create<Location>(Location, {
+            id: '4a7ba206-2376-4ed4-87ac-40e8a2df9341',
+            name: 'Location 1',
+          }),
+          GenericFactory.create<Location>(Location, {
+            id: 'f4000db9-3496-4b42-8bf0-2c336109ab78',
+            name: 'Location 2',
+          }),
+        ],
+      },
     });
-    mock = sinon.mock(mockRepository);
-    mock.expects('find').resolves([
-      GenericFactory.create<Location>(Location, {
-        id: 'a2891a7f-df6f-4748-8bcc-5c669e5f9bb3',
-        name: 'Location',
-      }),
-      GenericFactory.create<Location>(Location, {
-        id: 'b313b81c-6ed0-4f91-a93b-91b9dd0655a1',
-        name: 'Location 2',
-      })
-    ]);
+
     service = new LocationService(mockRepository);
   });
 
   it('should return locations', () => {
     expect(service.list()).resolves.toMatchObject([
       {
-        id: 'a2891a7f-df6f-4748-8bcc-5c669e5f9bb3',
-        name: 'Location',
+        id: '4a7ba206-2376-4ed4-87ac-40e8a2df9341',
+        name: 'Location 1',
       },
       {
-        id: 'b313b81c-6ed0-4f91-a93b-91b9dd0655a1',
+        id: 'f4000db9-3496-4b42-8bf0-2c336109ab78',
         name: 'Location 2',
-      }
-    ])
+      },
+    ]);
   });
 });
