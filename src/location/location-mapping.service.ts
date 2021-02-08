5fff5cea153fb5441bad4ab3ca7470f7daf4d00b
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-08 13:22:05 -0300

    test: location controller test

--- src/location/location-mapping.service.ts
+++ src/location/location-mapping.service.ts
@@ -1,4 +1,4 @@
-import { MappingService } from "src/common/mapping.service";
+import { MappingService } from "../common/mapping.service";
 import { LocationDto } from "./location.dto";
 import { Location } from "./location.entity";
 
